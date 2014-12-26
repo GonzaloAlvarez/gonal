ad04cf4080d5c846ce25188e9b0ac46f190d1b28
Gonzalo Alvarez <gonzaloab@gmail.com>
2014-12-26 05:40:09 -0500

    Fixed Phantom pre-existing messages. Also fixed order in task execution.

--- Phantom.js
+++ Phantom.js
@@ -3,6 +3,8 @@ var phantomjs = require('phantomjs');
 var path = require('path');
 
 var failed_on_exit = false;
+var tests_started = false;
+var preinit_messages = [];
 
 var phantom_options = {
 	path: path.dirname(phantomjs.path) + '/',
@@ -15,6 +17,12 @@ var phantom_options = {
 	}
 }
 
+var CONSTANTS = {
+	'INIT_TESTS': '__INIT_TESTS__**',
+	'END_TESTS_OK': '__END_TESTS_OK_**',
+	'END_TESTS_FAIL': '__END_TESTS_FAIL_**'
+}
+
 var args = process.argv.slice(2);
 
 phantom.create(function(ph) {
@@ -24,20 +32,41 @@ phantom.create(function(ph) {
 			width: 2000,
 			height: 1000
 		});
+		page.set('onError', function(msg, trace) {
+			console.log('FAILURE: ' + msg);
+			failed_on_exit = true;
+			ph.exit();
+		});
+		page.onConsoleMessage(function(msg) {
+			if(msg === CONSTANTS.END_TESTS_OK) {
+				ph.exit();
+			} else if(msg === CONSTANTS.END_TESTS_FAIL) {
+				failed_on_exit = true;
+				ph.exit();
+			} else if(msg === CONSTANTS.INIT_TESTS) {
+				if(preinit_messages.length > 0) {
+					console.error('FAILURE: There are messages queued comming from the application.');
+					preinit_messages.forEach(function(m) {
+						console.error('> ' + m);
+					});
+					failed_on_exit = true;
+					ph.exit();
+				}
+				tests_started = true;
+			} else {
+				if(tests_started === true) {
+					if(failed_on_exit === false) {
+						console.log(msg);
+					}
+				} else {
+					preinit_messages.push(msg);
+				}
+			}
+		});
 		page.open(url, function(status) {
 			if(status !== 'success') {
 				console.log('Failed loading site');
 			} else {
-				page.onConsoleMessage(function(msg) {
-					if(msg === 'AD') {
-						ph.exit();
-					} else if(msg == 'AF') {
-						failed_on_exit = true;
-						ph.exit();
-					} else {
-						console.log(msg);
-					}
-				});
 				setTimeout(function() {
 					if(args[0] === 'screenshot') {
 						page.render('build/live/screenshot.png', {format:'png'}, function(err) { ph.exit();});
@@ -51,28 +80,30 @@ phantom.create(function(ph) {
 						page.injectJs('bower_components/jasmine-jquery/lib/jasmine-jquery.js');
 						page.injectJs('home/js/main.spec.js');
 
-						var passed = page.evaluate(function(result) {
+						page.evaluate(function(c) {
 							jasmine.getEnv().addReporter(new jasmineReporters.TerminalReporter({
 								color: true,
 								verbosity: 3
 							}));
+							var CONSTANTS = JSON.parse(c);
 							jasmine.getEnv().addReporter({
 								jasmineStarted: function() { jasmine.getEnv().failedTests = false;},
 								specDone: function(spec) { if(spec.status === 'failed'){ jasmine.getEnv().failedTests = true;}},
 								jasmineDone: function() { 
 									if(jasmine.getEnv().failedTests) { 
-										console.log('AF');
+										console.log(CONSTANTS.END_TESTS_FAIL);
 									} else {
-										console.log('AD');
+										console.log(CONSTANTS.END_TESTS_OK);
 									}
 								}
 							});
 							setTimeout(function() {
+								console.log(CONSTANTS.INIT_TESTS);
 								var evt = document.createEvent('Event'); 
 								evt.initEvent('load', false, false);
 								window.dispatchEvent(evt);
 							}, 200);
-						});
+						}, null, JSON.stringify(CONSTANTS));
 					}
 				}, 200);
 			}
