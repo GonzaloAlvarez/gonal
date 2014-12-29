71fa03f56abe311b82c9587d6b6ae59635b3e1cb
Gonzalo Alvarez <gonzaloab@gmail.com>
2014-12-29 17:32:38 -0500

    Moved the screenshot task to a newly created jasmineinteg grunt task

--- grunt/jasmineinteg.js
+++ grunt/jasmineinteg.js
@@ -6,5 +6,10 @@
 // See: http://creativecommons.org/licenses/by-nc-nd/4.0/
 //
 module.exports = {
-    target: {}
+    screenshot: {
+        src: 'build/live/index.html',
+        dest: 'build/live/screenshot.png',
+        width: 2000,
+        height: 1000
+    }
 };
