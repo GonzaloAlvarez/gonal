9f75a48fbc3816c21eabe18528cb17897a3d9f69
Gonzalo Alvarez <gonzaloab@gmail.com>
2015-02-16 12:39:10 -0500

    Added parse source for abtests

--- js/common/abtests.js
+++ js/common/abtests.js
@@ -10,17 +10,33 @@ var abTests = function() {
 
 	gaParse.init();
 
+    var ABTests = Parse.Object.extend("ABTests");
+
 	var getURLParameter = function(name) {
 		return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||null;
 	};
+
+    var getParseTreatment = function(name, promise) {
+        var query = new Parse.Query(ABTests);
+        query.equalTo("Name", name);
+        query.first({
+            success: function(result) {
+                promise.resolve(result.get('Treatment'));
+            },
+            error: function(msg) {
+                promise.resolve('C');
+            }
+        });
+    };
 
 	return {
 		getTreatment: function(experiment) {
             var deferred = $.Deferred();
 			if(getURLParameter(experiment) === '1') {
 				deferred.resolve('T1');
-			}
-			deferred.resolve('C');
+			} else {
+                getParseTreatment(experiment, deferred);
+            }
             return deferred;
 		}
 	};
